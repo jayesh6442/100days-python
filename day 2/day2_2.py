2e316739520194ba7523c3a24498fe8d8ed8a687
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2023-12-16 18:55:59 +0530

    change

---
+++ day 2/day2_2.py
@@ -0,0 +1,17 @@
+#return sum of digit that are entered
+
+
+two_digit_number = input("enter the two digit nunbmer: ")
+
+first_number=two_digit_number[0]
+second_number=two_digit_number[1]
+
+print(first_number)
+print(second_number)
+
+
+#conver the string to integer
+
+resut = int(first_number)  + int (second_number)
+
+print("sum is: " +  str(resut))
