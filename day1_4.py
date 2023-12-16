9aa976c80c8f1ac70a6e70997adc426b570cf1c9
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2023-12-16 18:11:35 +0530

    none

--- day1_4.py
+++ day1_4.py
@@ -2,3 +2,11 @@
 #variable is kind of container for storing the thingss in programming the variable use for storing the data of the program
 
 
+print (len(input("enter the name to know the size of length: ")))
+
+
+
+#function return the lenght of the input size and we wil in fun hahaha
+
+
+name = input("enter the age: ")
---
+++ day1_5.py
@@ -0,0 +1,14 @@
+# variable exercise 
+print("enter the a")
+
+a = input()
+print ("enter the b")
+b = input()
+
+print("a is: "+a)
+print("b is: "+b)
+c=a
+a=b
+b=c
+print("after the change: "+a)
+print("after the change: "+b)
