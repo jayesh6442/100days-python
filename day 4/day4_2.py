d69962d7bade7283ca5d59db4527047e86f9146d
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2024-02-29 15:17:31 +0530

    ad code

--- day 4/day4_2.py
+++ day 4/day4_2.py
@@ -1,6 +1,4 @@
 import random
-
-
 choice = random.randint(0,1)
 if choice == 1:
     print("heads")
@@ -14,6 +12,8 @@ for i in list1:
 print(len(list1))
 #print(list1[8])  will give error like out of the range
 #lets make nested list
-
 list2 = [list1,4,5,6,4,3]
 print(list2)
+choise2= random.randint(1,100)
+print(choise2)
+
