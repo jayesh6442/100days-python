969080b632a70f5a43a353585125b26818472877
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2024-03-02 15:53:27 +0530

    new code with exercise

---
+++ practice_sets/hello.py
@@ -0,0 +1,60 @@
+# # print("hello jayesh")
+# # if 5  > 3:
+# #     print("we are in the wrong")
+# # else:
+# #     print("we are in the right")
+
+# # python variable and syntax and manymore things 
+# # we can define the variable using the name and variable data like
+# x= 10
+# print(x)
+
+
+# y = str("jayesh")
+# print(type(y))
+# print(y)
+
+
+# # python have naming convention different than other langs
+# myvar = "jayesh"
+# my_var = "jayesh"
+# _my_var = "jayesh"
+# myVar = "jayesh"
+# MYVAR = "jayesh"
+# myvar2 = "jayesh"
+
+
+# # we can assign multiple values to the variabe in python 
+# x,y,z ="orange","banana","apple"
+# print(x)
+# print(y)
+# print(z)
+
+# a = b = c = "jayesh the coder"
+# print(a)
+# print(b)
+# print(c)
+
+# fruits = ["apple","banana","chips"]
+# p,q,r = fruits
+# print(p)
+# print(q)
+# print(r)
+
+# print(p,q,r)
+# print(p+q+r)
+
+
+# j =5
+# e = 4
+# print(j+e)
+
+# # print(j+p)
+# #global variable name and scoop
+# x = "python in awesome"
+# def fu():
+#     x = "fantastic"
+#     print("what is",x)
+# fu()
+
+print("new chapter ")
